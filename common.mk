28a9e35bd18c4eb10d6d641c19111d6c26737349
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-21 23:31:03 -0400

    sm8250-common: Configure charging control HAL path
    Change-Id: Ib52af2e5fa0c3545e639d888375dbe0fa230afdd
    (cherry picked from commit 8a8a1e12830525db7d7aff17e5861b767714f82e)

--- common.mk
+++ common.mk
@@ -346,6 +346,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/qcom_battery/battery_charging_enabled)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm \
